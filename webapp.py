1755d643103112dc5607ffb50313da5a105fb614
Venla Heinonen <venlaheinonen12@gmail.com>
2025-02-05 00:26:04 +0000

    populations added to the search results

--- webapp.py
+++ webapp.py
@@ -10,50 +10,40 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row  
     return conn
 
+# create a route for the home page
 @app.route("/")
 def home():
     return render_template('home.html')
 
-# create new route for search result to show result on new page
+# create new route for search results to show result on new page
 @app.route("/search-result")
 def result():
-    query = request.args.get('query', '').strip()  # Lấy từ khóa tìm kiếm
+    query = request.args.get('query', '').strip()  # search for key words
     results = []
     if query:
         conn = get_db_connection()
         cursor = conn.cursor()
+        # retrieve information from the db for the SNP table
         cursor.execute("""
             SELECT STRONGEST_SNP_RISK_ALLELE,P_VALUE, CHR_ID, CHR_POS, MAPPED_GENE FROM association 
             WHERE STRONGEST_SNP_RISK_ALLELE LIKE ? OR REGION LIKE ? OR MAPPED_GENE LIKE ?
             ORDER BY STRONGEST_SNP_RISK_ALLELE
-        """, ('%' + query + '%', '%' + query + '%', '%' + query + '%'))
+        """, (query, query, query)) # with out '%'s the search is more accurate
         results = cursor.fetchall()
+        # retrieve infromation from the db to find all the possible populations
+        cursor.execute("""
+            SELECT DISTINCT population FROM association 
+            WHERE STRONGEST_SNP_RISK_ALLELE LIKE ? OR REGION LIKE ? OR MAPPED_GENE LIKE ?
+            """, (query, query, query))
+        populations = cursor.fetchall()
         conn.close()
 
-    return render_template('search-result.html', results=results, query=query)
+    return render_template('search-result.html', results=results, query=query, populations=populations)
     
 # create route for gene function page
 @app.route('/gene/<gene_name>')
-# def gene_detail(gene_name):
-#     conn = get_db_connection()
-#     cursor = conn.cursor()
-    
-
-#     cursor.execute("""
-#             SELECT * FROM gene_info 
-#             WHERE symbol = ?
-#         """, (gene_name,))
-#     gene = cursor.fetchone()
-
-    
-#     conn.close()
-
-#     if not gene:
-#         return "No gene information", 404
-
-#     return render_template('gene_detail.html', gene=gene)
 def gene(gene_name):
-    geneconnect = sqlite3.connect('Database/web_database.db')
+    geneconnect = get_db_connection()
     gene_table = pd.read_sql_query("SELECT * FROM gene_info", geneconnect)
     gene_table['symbol'] = gene_table['symbol'].astype(str)
 
@@ -76,7 +66,10 @@ def gene(gene_name):
         # if gene not found -> key error
         return f"We don't have any information about this gene called {gene_name}"
 
-
+# create route to population statistics page
+@app.route('/population_stats/<population_name>')
+def population(population_name):
+    return render_template('population_stats.html', name=population_name)
 
 
 @app.route("/about")
